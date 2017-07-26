9c13eb6a2caa6d2af400706ebe7e4cf8e2639632
Jhon Fredy Melo <jmelo@vertical.codaltec.com>
2017-07-26 09:26:07 -0500

    Test rutas/controller

---
+++ TestMVC/Controllers/HelloController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestMVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public string Index()
+        {
+            return "Este es el Index";
+        }
+
+        //public string Welcome(string name, int numTimes = 0)
+        //{
+        //    return HttpUtility.HtmlEncode("nombre "+name+" numtimes "+numTimes);
+        //}
+
+        public string Welcome(string name, int ID=1)
+        {
+            return HttpUtility.HtmlEncode("nombre " + name + " numtimes " + ID);
+        }
+    }
+}
